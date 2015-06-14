c3a706b54fb081986c05a5cd4b1f0161972c5948
crbirlad <crbirlad@crbirlad-MBP.local>
2015-06-14 17:19:27 +0200

    docs: remove unnecessary active flags
    Set active flag only on the (only) active flag

--- sample/script.js
+++ sample/script.js
@@ -4,8 +4,8 @@
   angular.module('dtsApp', ['ui.bootstrap', 'csbux.directives.dts'])
   .controller('MainCtrl', ['$scope', '$window', '$modal', function($scope, $window, $modal) {
 
-    $scope.tabsSample = [{title:"Tab1",content:"templates/template1.html",active:false,order:0}, {title:"Tab2",content:"templates/template2.html",active:true,order:1}, 
-    {title:"Tab3",content:"templates/template3.html",active:true,order:2}, {title:"Tab4",content:"templates/template4.html",active:true,order:3}];
+    $scope.tabsSample = [{title:"Tab1",content:"templates/template1.html", order:0}, {title:"Tab2",content:"templates/template2.html",active:true,order:1}, 
+    {title:"Tab3",content:"templates/template3.html",order:2}, {title:"Tab4",content:"templates/template4.html",order:3}];
   }]) 
 
   .controller('TestCtrl', ['$scope', '$window', '$modal', '$log', function($scope, $window, $modal, $log) {
