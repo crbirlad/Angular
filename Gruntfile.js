3462284ea8620afc2c893f5a0c9342a031fb39d4
crbirlad <crbirlad@crbirlad-MBP.local>
2015-06-25 21:35:38 +0200

    chore (versioning): version release files in dist
    version files in the dist folder in the filename
    split grunt targets in release and dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,10 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         csbuxBanner: '<%= pkg.name %> - v<%= pkg.version %> (<%= pkg.release %>) - <%= grunt.template.today("yyyy-mm-dd") %>  <%= pkg.copyright %>',
+        csbuxProdJs: 'csbux-<%= pkg.version %>.min.js',
+        csbuxProdCss: 'csbux-<%= pkg.version %>.min.css',
+        csbuxDevJs: 'csbux-dts.js',
+        csbuxDevCss: 'dtsStyle.css',
         ngtemplates: {
             myapp: {
                 options: {
@@ -26,7 +30,7 @@ module.exports = function(grunt) {
                         "src/dts/csbux-dts.js",                        
                         "build/csbux-tpl.js"
                     ],
-                    dest: 'build/csbux.min.js',
+                    dest: 'build/<%= csbuxProdJs %>',
                 },
             css: {
                     src: [
@@ -43,27 +47,75 @@ module.exports = function(grunt) {
             },
             js: {
                 files: {
-                    'build/csbux.min.js' : [ 'build/csbux.min.js' ]
+                    'build/<%= csbuxProdJs %>' : [ 'build/<%= csbuxProdJs %>' ]
                 }
             }
         },
         cssmin: {           
             css: {
                 src: 'build/csbux.concat.css',
-                dest: 'build/csbux.min.css'
+                dest: 'build/<%= csbuxProdCss %>'
             }
         },    
         copy: {
-          main: {
-            files: [
-              { src: 'src/dts/docs/index.html', dest:"build/index.html"},                            
+          release: {
+            files: [              
               { src: 'src/dts/docs/script.js', dest:"build/script.js"},                                          
               { src: 'src/dts/docs/templates/**', dest:"build/templates/", expand: true, flatten: true, filter:'isFile'},                            
-              { src: 'build/csbux.min.js', dest:"dist/csbux.min.js"},                            
-              { src: 'build/csbux.min.css', dest:"dist/csbux.min.css"},                            
+              { src: 'build/<%= csbuxProdJs %>', dest:"dist/<%= csbuxProdJs %>"},                            
+              { src: 'build/<%= csbuxProdCss %>', dest:"dist/<%= csbuxProdCss %>"},                            
             ],
           },
-        },           
+          dev: {
+            files: [
+                {expand: true, flatten: true, src: ['src/dts/docs/**.*'], dest: 'build/'},                
+                {expand: true, flatten: true, src: ['src/dts/docs/templates/**.*'], dest: 'build/templates'},                
+              // { src: 'src/dts/docs/index.html', dest:"build/index.html"},                            
+              // { src: 'src/dts/docs/script.js', dest:"build/script.js"},                                          
+              // { src: 'src/dts/docs/templates/**', dest:"build/templates/", expand: true, flatten: true, filter:'isFile'},                            
+              // { src: 'src/dts/docs/dtsStyle.css', dest:"build/dtsStyle.css"},                            
+              { src: 'src/dts/csbux-dts.js', dest:'build/csbux-dts.js'},                            
+              { src: 'templates/dts/csbux-dts-template.html', dest:"build/templates/dts/csbux-dts-template.html"},                            
+              
+            ],
+          },
+        },  
+        replace: {
+          release: {
+            options: {
+              patterns: [
+                {
+                  match: 'csbuxJs',
+                  replacement: '<%= csbuxProdJs %>'
+                },
+                {
+                  match: 'csbuxCss',
+                  replacement: '<%= csbuxProdCss %>'
+                }
+              ]
+            },
+            files: [
+              {expand: true, flatten: true, src: ['src/dts/docs/index.html'], dest: 'build/'}
+            ]
+          },
+          dev: {
+            options: {
+              patterns: [
+                {
+                  match: 'csbuxJs',
+                  replacement: '<%= csbuxDevJs %>'
+                },
+                {
+                  match: 'csbuxCss',
+                  replacement: '<%= csbuxDevCss %>'
+                }
+              ]
+            },
+            files: [
+              {expand: true, flatten: true, src: ['src/dts/docs/index.html'], dest: 'build/'}
+            ]
+          }
+        },         
         // Deletes all .js files, but skips min.js files 
         clean: {
           all: ["build", "dist"],
@@ -78,10 +130,15 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');    
+    grunt.loadNpmTasks('grunt-contrib-copy');  
+    grunt.loadNpmTasks('grunt-replace');  
     grunt.loadNpmTasks('grunt-contrib-clean');
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['clean:all', 'ngtemplates', 'concat', 'uglify', 'cssmin', 'copy', 'clean:postBuild']);
+    //dev builds
+    grunt.registerTask('default', ['copy:dev', 'replace:dev']);
+    grunt.registerTask('dev-clean', ['clean:all', 'copy:dev', 'replace:dev']);
+    
+    //release build
+    grunt.registerTask('release', ['clean:all', 'ngtemplates', 'concat', 'uglify', 'cssmin', 'copy:release', 'replace:release', 'clean:postBuild']);
 
 };
